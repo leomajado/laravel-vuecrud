531436b07c66580dc19c05e45e80e687241878e3
LeoM <leomaj@hotmail.com>
2022-03-04 03:13:38 +0100

    Laravel-Vue Init Commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Vue Component</div>
+
+                    <div class="card-body">
+
+                        You are logged in!
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
